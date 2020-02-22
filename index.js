f360bc334fc81b6d22a524af869e9db81d11fedb
Mohammad Nurdin (Zero) <mohammadnrdn@gmail.com>
2020-02-22 15:13:25 +0800

    update port number

--- index.js
+++ index.js
@@ -30,7 +30,9 @@ mysqlConnection.connect((err) => {
     }
 });
 
-app.listen(3000, ()=> console.log('Server running on port 3000!'));
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 app.get('/users', (req, res) => {
     mysqlConnection.query('SELECT * FROM users', (err, rows, fields) => {
