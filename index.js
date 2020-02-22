5cf7e53536049e3244cc3156eed5ff68fd0f24f1
Mohammad Nurdin (Zero) <mohammadnrdn@gmail.com>
2020-02-22 15:50:09 +0800

    fixed port issue

--- index.js
+++ index.js
@@ -30,8 +30,9 @@ mysqlConnection.connect((err) => {
     }
 });
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log('Express server listening on port', port)
 });
 
 app.get('/users', (req, res) => {
